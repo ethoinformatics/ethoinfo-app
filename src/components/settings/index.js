91afa2d9c598057d6f07b35605a09d77a762d34d
Sebastian Buys <sebastian.buys@gmail.com>
2017-07-02 00:00:06 -0400

    Add toggle for using map tiles locally or remotely. Update config reducer and add actions.

--- src/components/settings/index.js
+++ src/components/settings/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import { Page } from 'react-onsenui';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Settings = () => (
+import './settings.styl';
+
+import Boolean from '../form/fields/boolean/boolean';
+
+// Actions
+import {
+  setItem as setConfigItem,
+} from '../../redux/actions/config';
+
+
+// Redux setup
+const mapStateToProps = state =>
+  ({
+    useLocalTiles: state.config.useLocalTiles
+  });
+
+const mapDispatchToProps = dispatch => ({
+  setUseLocalTiles: (value) => {
+    dispatch(setConfigItem('useLocalTiles', value));
+  },
+});
+
+const Settings = ({ useLocalTiles, setUseLocalTiles }) => (
   <Page>
-    Settings view goes here
+    <div className={'settingsItemLabel'}>
+      Use local tiles?
+    </div>
+    <div className={'settingsItemInput'}>
+      <Boolean value={!!useLocalTiles} onChange={() => { setUseLocalTiles(!useLocalTiles); }} />
+      <div className={'settingsItemValue'}>{ useLocalTiles ? 'Yes' : 'No'}</div>
+    </div>
   </Page>
 );
 
-export default Settings;
+Settings.propTypes = {
+  useLocalTiles: PropTypes.bool,
+  setUseLocalTiles: PropTypes.func.isRequired
+};
+
+Settings.defaultProps = {
+  useLocalTiles: false
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Settings);
